7efd0dcf49bc3277151f51b357bfe1dba7b2dfd8
Richard Vsiansky <r.vsia@seznam.cz>
2020-06-17 13:34:30 +0200

    Replace config with JS file

---
+++ .releaserc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "branches": [
+      "master",
+      {"name": "beta", "channel": "beta", "prelease": "beta"}
+  ],
+  "extends": "semantic-release-monorepo",
+  prepare: [
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    {
+      'path': '@semantic-release/git',
+      'message': 'chore(' + output.package + '): release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+    }
+  ],
+  publish: [
+    '@semantic-release/npm'
+  ],
+  verifyConditions: [
+    '@semantic-release/npm',
+    '@semantic-release/git'
+  ],
+  monorepo: {
+    analyzeCommits: [
+      '@semantic-release/commit-analyzer'
+    ],
+    generateNotes: [
+      '@semantic-release/release-notes-generator'
+    ]
+  }
+};
